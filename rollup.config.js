4adc3a95ca16ecc8163f294f0c1133cda8229346
Galieva Yulia <Gal_Yulia@mail.ru>
2020-09-27 21:06:22 +0500

    replace custom fonts with import

--- rollup.config.js
+++ rollup.config.js
@@ -11,46 +11,52 @@ import copy from 'rollup-plugin-copy';
 
 
 const getPlugins = () => [
-  nodeResolve(),
-  babel({exclude: 'node_modules/**', babelHelpers: 'bundled'}),
-  postcss({
-    extract: false,
-    modules: true,
-    use: [],
-    plugins: [postcssImport(), postcssApply(), postcssVars(), url({ url: 'inline'})]
-  }),
-  typeScript({tsconfig: 'tsconfig.json'}),
-  eslint({include: 'src/'}),
-  copy({
-    targets: [
-        { src: 'src/assets/*', dest: 'dist/assets' },
-        // { src: 'demo-example/index.html', dest: 'demo-example/dist'},
-        { src: 'dist/emails-input.js', dest: 'demo-example/dist'}]}
-  ),
+    nodeResolve(),
+    babel({exclude: 'node_modules/**', babelHelpers: 'bundled'}),
+    postcss({
+        extract: false,
+        modules: true,
+        use: [],
+        plugins: [
+            postcssImport(),
+            postcssApply(),
+            postcssVars(),
+            url({url: 'inline'})]
+    }),
+    typeScript({tsconfig: 'tsconfig.json'}),
+    eslint({include: 'src/'}),
+    copy({
+            targets: [
+                {src: 'src/assets/*', dest: 'dist/assets'},
+                // { src: 'demo-example/index.html', dest: 'demo-example/dist'},
+                {src: 'demo-example/style.css', dest: 'demo-example/dist'},
+                {src: 'dist/emails-input.js', dest: 'demo-example/dist'}]
+        }
+    ),
 
-  // sizeSnapshot(), // напишет в консоль размер бандла
-  // terser(), // минификатор совместимый с ES2015+, форк и наследник UglifyES
-  // replace({
-  //     exclude: 'node_modules/**',
-  //     ENV: JSON.stringify(process.env.NODE_ENV || 'development'),
-  // }),
-  // (process.env.NODE_ENV === 'production' && uglify()),
+    // sizeSnapshot(), // напишет в консоль размер бандла
+    // terser(), // минификатор совместимый с ES2015+, форк и наследник UglifyES
+    // replace({
+    //     exclude: 'node_modules/**',
+    //     ENV: JSON.stringify(process.env.NODE_ENV || 'development'),
+    // }),
+    // (process.env.NODE_ENV === 'production' && uglify()),
 ];
 
 export default [{
-  input: 'src/index.ts',
-  output: {
-    file: 'dist/emails-input.js',
-    format: 'iife',
-    name: 'EmailsInput'
-  },
-  plugins: getPlugins()
-},{
-  input: 'demo-example/index.ts',
-  output: [{
-    file: 'demo-example/dist/example.js',
-    format: 'iife' ,
-    name: 'ExampleForm'
-  }],
-  plugins: getPlugins()
+    input: 'src/index.ts',
+    output: {
+        file: 'dist/emails-input.js',
+        format: 'iife',
+        name: 'EmailsInput'
+    },
+    plugins: getPlugins()
+}, {
+    input: 'demo-example/index.ts',
+    output: [{
+        file: 'demo-example/dist/example.js',
+        format: 'iife',
+        name: 'ExampleForm'
+    }],
+    plugins: getPlugins()
 }];
